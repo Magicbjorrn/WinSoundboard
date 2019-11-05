308dd486da6742d3c5a1c26bc70a9cbfac6898c1
magicbjorrn <bjorn.broer@dynalinq.eu>
2019-11-05 15:17:32 +0100

    Initial Commit

---
+++ Soundboard/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Soundboard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_AddSound = new System.Windows.Forms.Button();
+            this.pnl_SoundPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // btn_AddSound
+            // 
+            this.btn_AddSound.Location = new System.Drawing.Point(10, 10);
+            this.btn_AddSound.Name = "btn_AddSound";
+            this.btn_AddSound.Size = new System.Drawing.Size(100, 30);
+            this.btn_AddSound.TabIndex = 0;
+            this.btn_AddSound.Text = "Add A sound";
+            this.btn_AddSound.UseVisualStyleBackColor = true;
+            this.btn_AddSound.Click += new System.EventHandler(this.btn_AddSound_Click);
+            // 
+            // pnl_SoundPanel
+            // 
+            this.pnl_SoundPanel.Location = new System.Drawing.Point(10, 50);
+            this.pnl_SoundPanel.Name = "pnl_SoundPanel";
+            this.pnl_SoundPanel.Size = new System.Drawing.Size(650, 430);
+            this.pnl_SoundPanel.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 491);
+            this.Controls.Add(this.pnl_SoundPanel);
+            this.Controls.Add(this.btn_AddSound);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_AddSound;
+        private System.Windows.Forms.Panel pnl_SoundPanel;
+    }
+}
+
---
+++ Soundboard/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace Soundboard
+{
+    public partial class Form1 : Form
+    {
+        WindowsMediaPlayer wplayer = new WindowsMediaPlayer();
+
+        private string soundDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "soundfiles\\");
+
+        private List<Button> soundButtons = new List<Button>();
+        private Button[,] soundButtonsArray;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (!Directory.Exists(soundDirectory))
+            {
+                Directory.CreateDirectory(soundDirectory);
+            }
+
+            LoadSounds();
+        }
+
+        private void LoadSounds()
+        {
+            pnl_SoundPanel.Controls.Clear();
+            soundButtons.Clear();
+
+            string[] fileEntries = Directory.GetFiles(soundDirectory);
+
+            foreach (string fileDirectory in fileEntries) {
+                Button newButton = new Button();
+
+                pnl_SoundPanel.Controls.Add(newButton);
+
+                soundButtons.Add(newButton);
+
+                newButton.Text = Path.GetFileName(fileDirectory);
+                newButton.Size = new Size(100, 100);
+                newButton.Click += new EventHandler(btn_PlaySound_Click);
+            }
+
+            ConvertListToTwoDArray();
+            SetButtonPosition();
+        }
+
+        private void ConvertListToTwoDArray()
+        {
+            int nButtons = soundButtons.Count;
+            int nRows = nButtons / 4 + 1;
+
+            soundButtonsArray = new Button[nRows, 4];
+
+            for (int i = 0; i < soundButtonsArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < soundButtonsArray.GetLength(1); j++)
+                {
+                    int index = i * 4 + j;
+
+                    if (soundButtons.Count > index && soundButtons[index] != null)
+                    {
+                        soundButtonsArray[i, j] = soundButtons[index];
+                    }
+                }
+            }
+        }
+
+        private void SetButtonPosition()
+        {
+            for (int i = 0; i < soundButtonsArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < soundButtonsArray.GetLength(1); j++)
+                {
+                    if (soundButtonsArray[i, j] != null)
+                    {
+                        soundButtonsArray[i, j].Location = new Point(j * 100 + j * 10, i * 100 + i * 10);
+                    }
+                }
+            }
+        }
+        
+        private void btn_AddSound_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false; // allow/deny user to upload more than one file at a time
+            if (dialog.ShowDialog() == DialogResult.OK) // if user clicked OK
+            {
+                string sourceFileName = dialog.FileName;
+                string destinationFileName = Path.Combine(soundDirectory, Path.GetFileName(sourceFileName));
+
+                if (!File.Exists(destinationFileName))
+                {
+                    File.Copy(sourceFileName, destinationFileName);
+
+                    LoadSounds();
+                }
+            }
+        }
+
+        private void btn_PlaySound_Click(object sender, EventArgs e)
+        {
+            string fileName = Path.Combine(soundDirectory, (sender as Button).Text);
+
+            wplayer.controls.stop();
+            wplayer.URL = fileName;
+            wplayer.controls.play();
+        }
+    }
+}
